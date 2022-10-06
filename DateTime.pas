afc8b305f4d28c9e4423ac42b8ca555571e5d1c0
Слащев Константин <spasibo1911@yandex.ru>
2022-10-06 14:54:41 +0300

    Add a>b

--- DateTime.pas
+++ DateTime.pas
@@ -2,6 +2,6 @@
   var x:=DateTime.Now.Year;
   println('Високосный год',(x mod 4=0)and(x mod 100<>0)or(x mod 400=0));
   
-  var y:=13; //кол-во минут
-  print('Количество секунд в минуте:',y*60);
+  var (a,b):=readinteger2();
+  println('День а ближе дня b',a>b);
 end.
